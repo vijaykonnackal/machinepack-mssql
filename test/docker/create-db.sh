324f0d561b197f64f534daecd9873bda8aa93272
Vijay Konnackal <vijay.konnackal@gmail.com>
2020-04-09 16:27:08 +0530

    improved documentation

---
+++ test/docker/create-db.sh
@@ -0,0 +1,2 @@
+
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P $SA_PASSWORD -d master -i /tmp/sqlserver.setup.sql
--- test/docker/travis-test.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -P "$SA_PASSWORD" -i /machinepack-sqlserver/sqlserver.setup.sql
